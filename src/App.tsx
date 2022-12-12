a07ae09958f9d9610c78c99afc0e638f5cbba7b9
luisfop <luisfop.dev@gmail.com>
2022-12-12 10:30:29 -0300

    fix: fix navbar gap and mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
 
 
   return (
-    <div className="App">
-      <div className="section">
-        <h1>Hello world</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, architecto. Placeat vitae necessitatibus distinctio harum nobis, possimus modi alias vel quas totam facere.</p>
-      </div>
-
-      <button className="button">Click me</button>
+    <div>
+      <Navbar/>
     </div>
   )
 }
